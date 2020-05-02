3afcb23af994646fd0f97be81ef78c2493694784
heluansantos <heluan.caetano@gmail.com>
2020-05-01 23:14:15 -0300

    Validando cadastro

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Container, Content, Background } from "./styles";
 import logoImg from "../../assets/logo.svg";
 import { FiMail, FiLock, FiUser, FiArrowLeft } from "react-icons/fi";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 import { Form } from "@unform/web";
+import * as Yup from "yup";
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required("Nome obrigatório"),
+        email: Yup.string()
+          .required("E-mail obrigatório")
+          .email("Digite um e-mail válido"),
+        password: Yup.string().min(6, "No mínimo 6 dígitos"),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
