30daa6294a5390f962a48c3ccc7698ee3ff49fc0
heluansantos <heluan.caetano@gmail.com>
2020-05-03 13:52:33 -0300

    Criando hook de toast

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -8,7 +8,8 @@ import { Form } from "@unform/web";
 import { FormHandles } from "@unform/core";
 import * as Yup from "yup";
 import getValidationErrors from "../../utills/getValidationErrors";
-import { useAuth } from "../../hooks/AuthContext";
+import { useAuth } from "../../hooks/auth";
+import { useToast } from "../../hooks/toast";
 
 interface SignInFormData {
   email: string;
@@ -19,6 +20,7 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
   const { signIn } = useAuth();
+  const { addToast } = useToast();
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
@@ -35,7 +37,7 @@ const SignIn: React.FC = () => {
           abortEarly: false,
         });
 
-        signIn({
+        await signIn({
           email: data.email,
           password: data.password,
         });
@@ -45,10 +47,10 @@ const SignIn: React.FC = () => {
 
           formRef.current?.setErrors(errors);
         }
-        //disparar um toast
+        addToast();
       }
     },
-    [signIn]
+    [addToast, signIn]
   );
   return (
     <Container>
